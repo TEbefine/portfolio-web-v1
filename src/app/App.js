5a72d7b19eb90088a792938fc37b3d9abf9fa8a0
Teera Thongbai <teerathongbai@gmail.com>
2024-04-17 15:20:18 +0700

    remove unnecessary things

---
+++ src/app/App.js
@@ -0,0 +1,10 @@
+import React from "react";
+import { Route, BrowserRouter, Routes } from "react-router-dom";
+
+export default function App() {
+  return (
+    <div>
+      <h1>Hello would</h1>
+    </div>
+  );
+}
