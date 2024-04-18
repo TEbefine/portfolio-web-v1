53c45924f6d7a7d07b0239f89b9336e664a41f36
Teera Thongbai <teerathongbai@gmail.com>
2024-04-18 20:09:54 +0700

    first finish form head section

--- src/components/homepage.js
+++ src/components/homepage.js
@@ -4,19 +4,25 @@ import { Link } from "react-router-dom";
 function HomePage() {
   return (
     <header>
-      <nav>
-        <Link to="/pageTwo">Web Tech</Link>
-      </nav>
+      <Link to="/pageTwo" className="nav-link">
+        Web Tech{" "}
+        <img
+          src="./img/ArrowOutward.png"
+          alt="Arrow Icon"
+          width="40"
+          height="40"
+        />
+      </Link>
       <section>
         <h2>Nice to meet YOU</h2>
-        <p>
-          Welcome to <h1>TEERA's Portfolio Website</h1>.
-        </p>
-        <p>Which language would you prefer to use:</p>
-        <div>
-          <button>EN</button>
-          <button>TH</button>
-          <button>JP</button>
+        <div className="greeting">
+          <p>Welcome to TEERA's Portfolio Website.</p>
+          <div className="ChangeLanguage">
+            <p>Which language would you prefer to use:</p>
+            <button>EN</button>
+            <button>TH</button>
+            <button>JP</button>
+          </div>
         </div>
       </section>
     </header>
--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import React from "react";
 import { createRoot } from "react-dom/client";
+import './index.css';
 import { Provider } from "react-redux";
 import { store } from "./app/store";
 import App from "./app/App";
