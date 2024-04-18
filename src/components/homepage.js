4c1876936016730be91b8fd5e3282bf13d9dffd6
Teera Thongbai <teerathongbai@gmail.com>
2024-04-18 12:02:48 +0700

    Adjust the words

--- src/components/homepage.js
+++ src/components/homepage.js
@@ -3,12 +3,23 @@ import { Link } from "react-router-dom";
 
 function HomePage() {
   return (
-    <>
-      <div>
-        <Link to="/pageTwo">web detail</Link>
-        <p>ttt</p>
-      </div>
-    </>
+    <header>
+      <nav>
+        <Link to="/pageTwo">Web Tech</Link>
+      </nav>
+      <section>
+        <h2>Nice to meet YOU</h2>
+        <p>
+          Welcome to <h1>TEERA's Portfolio Website</h1>.
+        </p>
+        <p>Which language would you prefer to use:</p>
+        <div>
+          <button>EN</button>
+          <button>TH</button>
+          <button>JP</button>
+        </div>
+      </section>
+    </header>
   );
 }
 
