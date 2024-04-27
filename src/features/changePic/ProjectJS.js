9bfed338720b673425339f24b4af198c78df7e4f
Teera Thongbai <teerathongbai@gmail.com>
2024-04-27 13:03:28 +0700

    fix bug

--- src/features/changePic/ProjectJS.js
+++ src/features/changePic/ProjectJS.js
@@ -3,36 +3,45 @@ import styles from "./ProjectJs.module.css";
 
 function ProjectJS() {
   return (
-    <div className={styles.slider}>
-      <div className={styles.slides}>
+    <div className={styles.sliderOne}>
+      <div className={styles.slidesOne}>
         {/* radio buttons start */}
-        <input type="radio" name="radio-btn" id={styles.radio1} />
-        <input type="radio" name="radio-btn" id={styles.radio2} />
-        <input type="radio" name="radio-btn" id={styles.radio3} />
+        <input type="radio" name="radio-btnOne" id={styles.radio1One} />
+        <input type="radio" name="radio-btnOne" id={styles.radio2One} />
+        <input type="radio" name="radio-btnOne" id={styles.radio3One} />
         {/* radio buttons end */}
         {/* slide images start */}
-        <div className={`${styles.slide} ${styles.first}`}>
+        <div className={`${styles.slideOne} ${styles.firstOne}`}>
           <img src="/img/mystery-organism.png" alt="project new DNA" />
         </div>
-        <div className={styles.slide}>
+        <div className={styles.slideOne}>
           <img src="/img/gameJS.png" alt="project found hat" />
         </div>
-        <div className={styles.slide}>
+        <div className={styles.slideOne}>
           <img src="/img/credit-checker.png" alt="project credit checker" />
         </div>
         {/* slide images end */}
         {/* automatic navigation start */}
-        <div className={styles.navAuto}>
-          <div className={styles.autoBtn1}></div>
-          <div className={styles.autoBtn2}></div>
-          <div className={styles.autoBtn3}></div>
+        <div className={styles.navAutoOne}>
+          <div className={styles.autoBtn1One}></div>
+          <div className={styles.autoBtn2One}></div>
+          <div className={styles.autoBtn3One}></div>
         </div>
         {/* automatic navigation end */}
         {/* manal navigation start */}
-        <div className={styles.navManual}>
-          <label htmlFor={styles.radio1} className={styles.manualBtn}></label>
-          <label htmlFor={styles.radio2} className={styles.manualBtn}></label>
-          <label htmlFor={styles.radio3} className={styles.manualBtn}></label>
+        <div className={styles.navManualOne}>
+          <label
+            htmlFor={styles.radio1One}
+            className={styles.manualBtnOne}
+          ></label>
+          <label
+            htmlFor={styles.radio2One}
+            className={styles.manualBtnOne}
+          ></label>
+          <label
+            htmlFor={styles.radio3One}
+            className={styles.manualBtnOne}
+          ></label>
         </div>
         {/* manal navigation end */}
       </div>
